6cd74b7c392e2d8d3651477c7579ea8a32b4de33
GeonU-be <timepublic00@gmail.com>
2025-12-08 11:06:35 +0900

    openai_llm 추가

---
+++ app/services/llm/openai_llm.py
@@ -0,0 +1,26 @@
+from langchain_openai import ChatOpenAI
+from langchain.schema import SystemMessage, HumanMessage
+
+
+def get_llm(api_key: str):
+    return ChatOpenAI(
+        model="gpt-4o",
+        api_key=api_key,
+        # reasoning_effort="low",
+        # verbosity="low"
+    )
+
+
+async def call_llm(api_key, system_prompt, input_prompt):
+    messages = [
+        SystemMessage(content=system_prompt),
+        HumanMessage(content=input_prompt),
+    ]
+
+    result = await get_llm(api_key=api_key).ainvoke(messages)
+
+    # 결과값의 .content: 출력물
+    return result.content.strip()
+
+
+print("load openai llm")
